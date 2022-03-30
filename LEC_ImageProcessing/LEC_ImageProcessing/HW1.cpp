3f9d38136792c7d7d181db54d5a46487f4ecd7ee
KimJinSeong-Git <acmiheee@gmail.com>
2022-03-30 11:28:33 +0900

    Feat: Generate Raw file - 과제 1번 - Smooth Ramp를 적용한 raw 파일 생성

---
+++ LEC_ImageProcessing/LEC_ImageProcessing/HW1.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <fstream>
+#include <Windows.h>
+#include <math.h>
+
+using namespace std;
+
+// Proto type
+int smoothRamp(int col);
+double equation(int col, double x1, double y1, double x2, double y2);
+
+// Main
+int main() {
+	int imageSize = 512;
+	int bright;
+
+	// 메모리 할당
+	char** imageBuffer;
+	imageBuffer = (char**)malloc(sizeof(char*) * imageSize);
+	for (int i = 0; i < imageSize; i++) {
+		imageBuffer[i] = (char*)malloc(sizeof(char) * imageSize);
+	}
+
+	for (int col = 0; col < imageSize; col++) {
+		bright = smoothRamp(col);
+		for (int row = 0; row < imageSize; row++) {
+			imageBuffer[row][col] = bright;
+		}
+	}
+	
+	// 파일 저장
+	FILE* fp;
+	fopen_s(&fp, "HW1-1.raw", "wb");
+
+	fwrite(imageBuffer, sizeof(char), imageSize* imageSize, fp);
+	fclose(fp);
+	
+	// 메모리 해제
+	for (int i = 0; i < imageSize; i++) {
+		free(imageBuffer[i]);
+	}
+	free(imageBuffer);
+
+	return 0;
+}
+
+int smoothRamp(int col) {
+	double brightness;
+	if (col < 100)
+		brightness = 120;
+	else if (col < 200)
+		brightness = equation(col, 100, 120, 200, 135);
+	else if (col < 280)
+		brightness = equation(col, 200, 135, 280, 225);
+	else if (col < 300)
+		brightness = equation(col, 280, 225, 300, 235);
+	else
+		brightness = 240;
+
+	return (int)brightness;
+}
+
+double equation(int col, double x1, double y1, double x2, double y2) {
+	return ((y2 - y1) / (x2 - x1))*(col - x1) + y1;
+}
