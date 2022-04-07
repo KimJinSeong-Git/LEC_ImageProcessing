72c5b31ce5ad95095c2b6b6ca59979b59a0f7674
Kim Jin-Seong <acmiheee@gmail.com>
2022-04-07 23:33:34 +0900

    Feat: img to np - 이미지 배열로 변환

--- HW2_Histogram/HW2.py
+++ HW2_Histogram/HW2.py
@@ -2,9 +2,11 @@ import os
 import numpy as np
 from PIL import Image
 
+# load image
 file = "F:/github/LEC_ImageProcessing/HW2_Histogram/lena_bmp_512x512_new.bmp"
-imageData = np.zeros((512, 512, 3), dtype=np.uint8)
-imageData[0:256, 0:256] = [255, 0, 0]
-img = Image.fromarray(imageData, "RGB")
-img.save(file)
-img.show()
+img = Image.open(file)
+#img.show()
+
+# image to numpy array
+img_np = np.array(img)
+print(img_np.shape)
