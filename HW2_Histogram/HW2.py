36e02ea57b156ece487ae3d00e27113d75eb422e
KimJinSeong-Git <acmiheee@gmail.com>
2022-04-08 16:18:32 +0900

    Fix: root - 파일 경로 상대경로로 수정

--- HW2_Histogram/HW2.py
+++ HW2_Histogram/HW2.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 from PIL import Image
 
 # load image
-file = "F:/github/LEC_ImageProcessing/HW2_Histogram/lena_bmp_512x512_new.bmp"
+file = "./HW2_Histogram/lena_bmp_512x512_new.bmp"
 img = Image.open(file)
 #img.show()
 
