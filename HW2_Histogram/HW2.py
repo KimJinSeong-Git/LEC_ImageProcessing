940225fa81838b65910301451b845e0b4a41df0e
KimJinSeong-Git <acmiheee@gmail.com>
2022-04-08 16:41:52 +0900

    Feat: find sum - 히스토그램 누적합 구하기

--- HW2_Histogram/HW2.py
+++ HW2_Histogram/HW2.py
@@ -13,14 +13,21 @@ img_np = np.array(img)
 
 # count bright
 bright = np.zeros(255, np.uint16)
-count = 0
 for row in range(img_np.shape[0]):
     for col in range(img_np.shape[1]):
         curPix = img_np[row, col]
         bright[curPix] = bright[curPix] + 1
-        count = count + 1
 
 # print graph
 x = np.arange(255)
 plt.bar(x, bright, width=1.0)
 plt.show()
+
+# Histogram Equalization
+# - 누적합
+sum = np.zeros(255, np.uint32)
+sum[0] = bright[0]
+for pixBright in range(1, 255):
+    sum[pixBright] = sum[pixBright-1] + bright[pixBright]
+
+print(sum)
