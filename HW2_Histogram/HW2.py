4cb874b10c107820af80079d71181d07be7ac946
Kim Jin-Seong <acmiheee@gmail.com>
2022-04-11 14:54:07 +0900

    Feat: All function - 과제 완료

--- HW2_Histogram/HW2.py
+++ HW2_Histogram/HW2.py
@@ -17,6 +17,10 @@ def printGraph(histogram):
     plt.bar(x, histogram, width=1.0)
     plt.show()
 
+def showImg(imgArray):
+    image = Image.fromarray(imgArray)
+    image.show()
+
 def prefixSum(histogram):
     sum = np.zeros(255, np.uint32)
     sum[0] = histogram[0]
@@ -24,26 +28,57 @@ def prefixSum(histogram):
         sum[i] = sum[i-1] + histogram[i]
     return sum
 
-def normalizeSum(histogram):
+def createLUT(histogram):
     # 누적합 구하기
     sum = prefixSum(histogram)
     nb_pix = sum[-1]
-    norm_sum = np.zeros(255, np.uint32)
+    lut = np.zeros(255, np.uint32)
     for i in range(255):
-        # 정규화된 누적 합 = (누적합 / 픽셀 수) * 최대 명도 & 반올림을 위한 +0.5
-        norm_sum[i] = (sum[i] / nb_pix)*255 + 0.5 
+        # 정규화된 누적 합 = (누적합 / 픽셀 수) * 최대 명도 & 반올림을 위한 +0.5 = LUT
+        lut[i] = (sum[i] / nb_pix)*255 + 0.5 
 
-    return norm_sum
+    return lut
 
-def histogramEQ(imgArray):
-    norm_sum = normalizeSum(basic_histogram)
-    img_EQ = np.array(img)
+def createEQ(imgArray):
+    image_EQ = imgArray.copy()
+    basic_histogram = createHistogram(image_EQ)
+    lut = createLUT(basic_histogram)
 
-    for row in range(img_EQ.shape[0]):
-        for col in range(img_EQ.shape[1]):
-            curBright = img_EQ[row, col]
-            img_EQ[row, col] = norm_sum[curBright]
-    return img_EQ
+    for row in range(image_EQ.shape[0]):
+        for col in range(image_EQ.shape[1]):
+            curBright = image_EQ[row, col]
+            image_EQ[row, col] = lut[curBright]
+
+    return image_EQ
+
+def createBasicContrastStretching(imgArray):
+    image_Stretching = imgArray.copy()
+    low = np.min(image_Stretching)
+    high = np.max(image_Stretching)
+    mult = 255 / (high - low)
+    for row in range(image_Stretching.shape[0]):
+        for col in range(image_Stretching.shape[1]):
+            image_Stretching[row, col] = (image_Stretching[row, col] - low) * mult
+    
+    return image_Stretching
+
+def createEndsIn(imgArray):
+    image_EndsIn = imgArray.copy()
+    low = 40
+    high = 190
+    mult = 255 / (high - low)
+    for row in range(image_EndsIn.shape[0]):
+        for col in range(image_EndsIn.shape[1]):
+            x = image_EndsIn[row, col]
+            if x<low:
+                image_EndsIn[row, col] = 0
+            elif x>high:
+                image_EndsIn[row, col] = 255
+            else:  
+                image_EndsIn[row, col] = (image_EndsIn[row, col] - low) * mult
+    return image_EndsIn
+
+
 
 if __name__ == '__main__':
     # load image
@@ -56,10 +91,23 @@ if __name__ == '__main__':
 
     # --------------- 1. Histogram ---------------
     basic_histogram = createHistogram(img_np)
-    printGraph(basic_histogram)
 
     # --------------- 2. Histogram Equalization ---------------
-    img_EQ = 
+    img_EQ = createEQ(img_np)
     EQ_histogram = createHistogram(img_EQ)
-    printGraph(EQ_histogram)
+
     # --------------- 3. Basic Contrast Stretching ---------------
+    img_ContrastStretching = createBasicContrastStretching(img_np)
+    contrast_histogram = createHistogram(img_ContrastStretching)
+
+    # --------------- 4. Ends-in Contrast Stretching ---------------
+    img_EndsIn = createEndsIn(img_np)
+    endsIn_histogram = createHistogram(img_EndsIn)
+    # showImg(img_np)
+    # showImg(img_EQ)
+    # showImg(img_ContrastStretching)
+    # showImg(img_EndsIn)
+    printGraph(basic_histogram)
+    printGraph(EQ_histogram)
+    printGraph(contrast_histogram)
+    printGraph(endsIn_histogram)
